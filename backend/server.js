f8f1cfa6b917629531610e3b9b2ab1d96194d465
Misoko <misoko.hofmann@gmail.com>
2024-11-11 18:12:54 +0100

    messages api (send and get)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
@@ -11,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with json payloads from body
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 app.listen(PORT, () => {
