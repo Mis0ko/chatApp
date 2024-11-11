6c098db4d15b7df8410f9992559c1c83779aafb9
Misoko <misoko.hofmann@gmail.com>
2024-11-11 17:04:20 +0100

    add salt password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import bcrypt from "bcryptjs"
 export const signup = async (req,res) => {
    try {
      const {fullName, username, password, confirmPassword, gender} = req.body;
@@ -16,24 +16,31 @@ export const signup = async (req,res) => {
      }
 
      // HASH PASSWORD
+     const salt = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salt);
 
      const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
      const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
-
+     
      const newUser = new User({
         fullName,
         username,
-        password,
+        password: hashedPassword,
         gender,
         profilePic: gender==="male" ? boyProfilePic: girlProfilePic
      })
-
-     await newUser.save();
-     res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.username,
-        profilePic: newUser.profilePic
-     })
+     if (newUser){
+        // generate JWR token
+        await newUser.save();
+        res.status(201).json({
+           _id: newUser._id,
+           fullName: newUser.username,
+           profilePic: newUser.profilePic
+        })
+     }
+     else{
+        res.status(400).json({error:"Invalid user data"})
+     }
    } catch (error) {
     console.log("Error in signup controller", error.message)
     res.status(500).json({error:"Internal Server Error"})
