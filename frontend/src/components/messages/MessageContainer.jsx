645e26efa4711fd4d17425bce882f8e02f86ddd8
Misoko <misoko.hofmann@gmail.com>
2024-11-13 21:22:29 +0100

    starter Messages

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Messages from "./Messages";
 
 const MessageContainer = () => {
-  return <div>MessageContainer</div>;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <>
+        {/* Headers */}
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          <span className="label-text">To:</span>{" "}
+          <span className="text-gray-900 font-bold"> John Doe</span>
+        </div>
+
+        <Messages />
+      </>
+    </div>
+  );
 };
 
 export default MessageContainer;
