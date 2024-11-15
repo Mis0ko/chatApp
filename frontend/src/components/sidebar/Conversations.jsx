6845f70f05c93d5e899186ea3a0f0ae1c1f95e85
Misoko <misoko.hofmann@gmail.com>
2024-11-15 18:23:45 +0100

    Link sidebar conversation with backend

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,31 @@
+import useGetConversations from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS ", conversations);
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => {
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversation.length - 1}
+        />;
+      })}
+
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+      {/* <Conversation />
       <Conversation />
       <Conversation />
       <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      <Conversation /> */}
+      <Conver sation />
     </div>
   );
 };
