1d84c6fcc3eb0806cdbdcb8746caa104d14f5c9d
Misoko <misoko.hofmann@gmail.com>
2024-11-15 13:01:25 +0100

    logout with authcontext and auth provider

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,8 +1,10 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 // import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
   //   const { loading, logout } = useLogout();
+  const { loading, logout } = useLogout();
 
   return (
     // <div className="mt-auto">
@@ -16,7 +18,14 @@ const LogoutButton = () => {
     //   )}
     // </div>
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner loading-lg"></span>
+      )}
     </div>
   );
 };
