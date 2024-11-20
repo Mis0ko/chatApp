a8c9f9bf8b29f3ce0d88e173f7c98ce37e8fbe26
Misoko <misoko.hofmann@gmail.com>
2024-11-20 20:14:48 +0100

    create Server to deal Sockets with http server

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ import userRoutes from "./routes/user.routes.js";
 
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 
-const app = express();
+//const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +25,7 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server running on port ${PORT}`)
 })
---
+++ backend/socket/socket.js
@@ -0,0 +1,25 @@
+import {Server} from 'socket.io'
+import http from 'http';
+import express from 'express';
+
+const app = express();
+
+const server = http.createServer(app) // englobe le serveur express par un server socket
+const io = new Server(server, {
+    cors:{
+        origin:["http://localhost:3000"],
+        methods:["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket)=> {
+    console.log("a user connected", socket.id)
+
+
+    socket.on("disconnect", ()=> {
+        console.log("user disconnedted", socket.id)
+    })
+
+})
+
+export {app, io, server}
