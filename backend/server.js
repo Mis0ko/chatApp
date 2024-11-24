fac1233f7a5398f36518c86cd7f1bfca00b6448b
Misoko <misoko.hofmann@gmail.com>
2024-11-24 14:45:03 +0100

    built version, ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
-
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -15,6 +15,10 @@ import { app, server } from "./socket/socket.js";
 //const app = express();
 const PORT = process.env.PORT || 5000;
 
+// give absolute path to the root folder
+const __dirname = path.resolve();
+
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with json payloads from body
@@ -23,6 +27,15 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// use to serve static file (html, css, js)
+// frontend build in production in dist folder
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+// every routes that in not into "/api/*" is redirected to frontend index.html (deployed version)
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+});
 
 
 server.listen(PORT, () => {
